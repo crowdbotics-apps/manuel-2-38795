4883c55490fe85587dd99943348d3f267fc0deb2
Crowdbotics <engineering@crowdbotics.com>
2023-02-07 18:01:53 +0000

    Update components

---
+++ screens/Screen4(8:341)1293309_8_341/index.js
@@ -0,0 +1,548 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_8_342}>
+        <ImageBackground
+          source={{
+            uri:
+              "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/85420718-1789-4393-9dca-83192fa63a48"
+          }}
+          style={styles.ImageBackground_8_343}
+        />
+        <View style={styles.View_8_345}>
+          <ImageBackground
+            source={{
+              uri:
+                "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/eed7ce09-c23b-4468-b80a-6bde4258ba78"
+            }}
+            style={styles.ImageBackground_8_346}
+          />
+          <View style={styles.View_8_347}>
+            <View style={styles.View_8_348}>
+              <View style={styles.View_8_349}>
+                <ImageBackground
+                  source={{
+                    uri:
+                      "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/da6f4740-f213-4901-85e5-84934373ae40"
+                  }}
+                  style={styles.ImageBackground_8_350}
+                />
+                <View style={styles.View_8_352}>
+                  <View style={styles.View_8_353}>
+                    <ImageBackground
+                      source={{
+                        uri:
+                          "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/28ee4781-fe1a-4525-ac78-dbcfd16b2aca"
+                      }}
+                      style={styles.ImageBackground_8_354}
+                    />
+                  </View>
+                </View>
+              </View>
+            </View>
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/d0b3631a-fdd2-4542-b80a-d9708e328121"
+              }}
+              style={styles.ImageBackground_8_355}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/25a537b3-61d3-4f4d-a30e-9d2420f41afd"
+              }}
+              style={styles.ImageBackground_8_356}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/9333446a-4d40-4d7b-848a-9ed36dacd5d2"
+              }}
+              style={styles.ImageBackground_8_357}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/aa90b4cb-a5db-4063-8c28-1c5603812a74"
+              }}
+              style={styles.ImageBackground_8_369}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/8e811229-7c00-446b-916f-594a96eda3a9"
+              }}
+              style={styles.ImageBackground_8_373}
+            />
+            <View style={styles.View_8_379}>
+              <ImageBackground
+                source={{
+                  uri:
+                    "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/1b61139a-9dd8-4330-9b38-36c2bc1b8823"
+                }}
+                style={styles.ImageBackground_8_380}
+              />
+            </View>
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/e771040b-01c8-488e-a41d-b6eb5aec8166"
+              }}
+              style={styles.ImageBackground_8_955}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/67f3edbb-b6ef-49e2-8dc0-8ae2f0f1bcf5"
+              }}
+              style={styles.ImageBackground_8_973}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/2ce13fd7-cb97-4d90-a210-e2515768a349"
+              }}
+              style={styles.ImageBackground_8_980}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/8680749c-fc40-4ed1-8ea5-8de2265d972e"
+              }}
+              style={styles.ImageBackground_8_981}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/0abe4d11-be6c-4b2e-85fc-72e5a92e5c83"
+              }}
+              style={styles.ImageBackground_8_982}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/6cafe77c-56a3-40f4-b1ce-f05a85660f07"
+              }}
+              style={styles.ImageBackground_8_983}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/482ccea7-da21-4838-93d2-62aaeac91539"
+              }}
+              style={styles.ImageBackground_8_984}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/86c984eb-8eb7-4c0c-bb1a-bef61a3cafef"
+              }}
+              style={styles.ImageBackground_8_985}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/c850a99a-221f-4bff-931f-1530493a488d"
+              }}
+              style={styles.ImageBackground_8_986}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/8d126044-7e83-4a3e-a093-e42a71e27fc4"
+              }}
+              style={styles.ImageBackground_8_999}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/3a3a47ac-daf2-42d7-8ab2-01815454ef52"
+              }}
+              style={styles.ImageBackground_8_1002}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/f25513e3-3099-4e2d-b442-a9ab231ccb82"
+              }}
+              style={styles.ImageBackground_8_1004}
+            />
+            <View style={styles.View_8_1005}>
+              <ImageBackground
+                source={{
+                  uri:
+                    "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/1c7691c1-27f2-4d6f-8ae8-ba224328f034"
+                }}
+                style={styles.ImageBackground_8_1006}
+              />
+            </View>
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/927cf75d-04b9-412a-9c5a-5a2ea5b04f56"
+              }}
+              style={styles.ImageBackground_8_1008}
+            />
+            <ImageBackground
+              source={{
+                uri:
+                  "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/1f96144b-5c70-49dd-a230-8d4ceab1e65d"
+              }}
+              style={styles.ImageBackground_8_1009}
+            />
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  View_2: { height: hp("127%") },
+  View_8_342: {
+    width: wp("100%"),
+    height: hp("127%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_343: {
+    width: wp("100%"),
+    height: hp("127%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  View_8_345: {
+    width: wp("100%"),
+    height: hp("127%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_346: {
+    width: wp("100%"),
+    height: hp("127%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  View_8_347: {
+    width: wp("100%"),
+    height: hp("127%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  View_8_348: {
+    width: wp("100%"),
+    height: hp("53%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  View_8_349: {
+    width: wp("100%"),
+    height: hp("53%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_350: {
+    width: wp("100%"),
+    height: hp("53%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  View_8_352: {
+    width: wp("100%"),
+    height: hp("53%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  View_8_353: {
+    width: wp("151%"),
+    height: hp("59%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_354: {
+    width: wp("151%"),
+    height: hp("59%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_355: {
+    width: wp("100%"),
+    height: hp("127%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_356: {
+    width: wp("100%"),
+    height: hp("82%"),
+    top: hp("46%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_357: {
+    width: wp("41%"),
+    height: hp("4%"),
+    top: hp("57%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("7%")
+  },
+  ImageBackground_8_369: {
+    width: wp("5%"),
+    height: hp("2%"),
+    top: hp("56%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("87%")
+  },
+  ImageBackground_8_373: {
+    width: wp("12%"),
+    height: hp("2%"),
+    top: hp("67%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("44%")
+  },
+  View_8_379: {
+    width: wp("91%"),
+    height: hp("40%"),
+    top: hp("72%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("4%")
+  },
+  ImageBackground_8_380: {
+    width: wp("91%"),
+    height: hp("40%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_955: {
+    width: wp("41%"),
+    height: hp("2%"),
+    top: hp("61%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("7%")
+  },
+  ImageBackground_8_973: {
+    width: wp("8%"),
+    height: hp("2%"),
+    top: hp("61%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("85%")
+  },
+  ImageBackground_8_980: {
+    width: wp("3%"),
+    height: hp("2%"),
+    top: hp("59%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("75%")
+  },
+  ImageBackground_8_981: {
+    width: wp("3%"),
+    height: hp("2%"),
+    top: hp("59%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("79%")
+  },
+  ImageBackground_8_982: {
+    width: wp("3%"),
+    height: hp("2%"),
+    top: hp("59%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("82%")
+  },
+  ImageBackground_8_983: {
+    width: wp("3%"),
+    height: hp("2%"),
+    top: hp("59%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("86%")
+  },
+  ImageBackground_8_984: {
+    width: wp("3%"),
+    height: hp("2%"),
+    top: hp("59%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("90%")
+  },
+  ImageBackground_8_985: {
+    width: wp("69%"),
+    height: hp("8%"),
+    top: hp("115%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("16%")
+  },
+  ImageBackground_8_986: {
+    width: wp("30%"),
+    height: hp("2%"),
+    top: hp("118%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("35%")
+  },
+  ImageBackground_8_999: {
+    width: wp("79%"),
+    height: hp("1%"),
+    top: hp("68%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("10%")
+  },
+  ImageBackground_8_1002: {
+    width: wp("15%"),
+    height: hp("0%"),
+    top: hp("49%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("43%")
+  },
+  ImageBackground_8_1004: {
+    width: wp("9%"),
+    height: hp("5%"),
+    top: hp("5%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("8%")
+  },
+  View_8_1005: {
+    width: wp("3%"),
+    height: hp("3%"),
+    top: hp("6%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("11%")
+  },
+  ImageBackground_8_1006: {
+    width: wp("3%"),
+    height: hp("3%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%")
+  },
+  ImageBackground_8_1008: {
+    width: wp("9%"),
+    height: hp("5%"),
+    top: hp("5%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("83%")
+  },
+  ImageBackground_8_1009: {
+    width: wp("6%"),
+    height: hp("3%"),
+    top: hp("6%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("85%")
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
